cb662dc34f358ca7afae9f6d67fc22711af6c794
Fury_Craft <gaming.tv.79.64@gmail.com>
2024-03-06 16:53:40 +0100

    Added default languages

---
+++ english/paragraph_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['forget_password'] = 'Enter your email address and your password will be reset and emailed to you.';
+$lang['account_limit'] = 'You are about to reach the limit of 3 free hosting accounts per user. Please consider upgrading to';
+$lang['account_limit_crossed'] = 'You have reached the maximum limit of 3 free hosting accounts per user. Please upgrade to';
+$lang['better_service'] = 'for better service.';
+$lang['nothing_to_show'] = 'Nothing to show.';
+$lang['account_warning'] = 'Please remove all domains and subdomains before deactivating your account otherwise all domains and subdomains on this account will be locked forever.';
+$lang['cpanel_login'] = 'We\'re now going to redirect you to cPanel.';
+$lang['account_created'] = 'Everything has been setup correctly.';
+$lang['search_note'] = 'The search result will be displayed here once a domain name is searched.';
+$lang['search_error'] = 'Sorry! This domain name does not seem to be assosiated with any of our web hosting accounts.';
+$lang['oops_note'] = 'Oops… You just found an error page';
+$lang['user_inactive_note'] = 'We are sorry but your account is inactive, please check your inbox.';
+$lang['err_404_note'] = 'We are sorry but the page you are looking for was not found.';
+$lang['err_500_note'] = 'We are sorry but our service is not available right now.';
+$lang['err_503_note'] = 'We are sorry but access to the page you are looking for is forbidden.';
+$lang['account_pending'] = 'This hosting account is pending and will be activated soon.';
+$lang['account_reactivating'] = 'This hosting account is reactivating and will be activated soon.';
+$lang['account_deactivating'] = 'This hosting account is deactivating and will be deactivated soon.';
+$lang['account_suspended'] = 'This hosting account is suspended due to the reason provided in your email and will be completely removed with in 30 days.';
+$lang['account_deactivated'] = 'This hosting account is deactivated and will be completely removed with in 30 days.';
+$lang['account_note'] = 'Some of the hosting features may not work. It may take up to 72 hours for the hosting account to work properly.';
+$lang['no_reply_found'] = 'No reply found.';
+$lang['ticket_closed'] = 'Ticket has been closed. Click';
+$lang['to_reopen'] = 'to re-open.';
